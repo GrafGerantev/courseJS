bab3501a1cb2884e04ec1312453fa3ac6bcbc898
GrafGer <mr.gerantev@mail.ru>
2020-09-13 19:02:59 +0300

    Создание модального окна

--- food/js/main.js
+++ food/js/main.js
@@ -88,6 +88,43 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
+	// Модальное окно Раздел 3 Видео 43
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]');
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', () => {
+			modal.classList.add('show');
+			modal.classList.remove('hide');
+			document.body.style.overflow = 'hidden';
+		});
+	});
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';
+	}
+
+	modalCloseBtn.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', (e) => {
+
+		if (e.target === modal) {
+			closeModal();
+		}
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === "Escape" && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	//Вызов функций
+
 	hideTabContent();
 	showTabContent();
 	setClock('.timer', deadline);
