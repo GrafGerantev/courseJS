56e392f44c30911465a7b57eac3b25846c57a00f
GrafGer <mr.gerantev@mail.ru>
2020-10-20 16:03:10 +0300

    1

--- food/js/main.js
+++ food/js/main.js
@@ -126,7 +126,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	const modalTimerId = setTimeout(openModal, 5000);
+	/* 	const modalTimerId = setTimeout(openModal, 5000); */
 
 	function showModalByScroll() {
 		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -137,10 +137,101 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	window.addEventListener('scroll', showModalByScroll);
 
+	// Используем классы для карточек
+
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes /*Rest оператор*/ ) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 79;
+			this.changeToRUB();
+		}
+		changeToRUB() {
+			this.price = this.price * this.transfer;
+		}
+		render() {
+			const element = document.createElement('div');
+			if (this.classes.length === 0) {
+				this.classes = 'menu__item'; /*Записываем в свойство*/
+				element.classList.add(this.classes);
+			} else {
+				this.classes.forEach(className => element.classList.add(className)); /* 49. Rest оператор и параметры по умолчанию (ES6) */
+			}
+			element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+				</div>
+				
+`;
+			this.parent.append(element);
+
+		}
+	}
+
 	//Вызов функций
 
 	hideTabContent();
 	showTabContent();
 	setClock('.timer', deadline);
-	console.log(event);
+	//console.log(event);
+
+	/* const div = new MenuCard();
+	div.MenuCard().render(); */
+
+	new MenuCard(
+		"img/tabs/vegy.jpg",
+		"vegy",
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		9,
+		".menu .container",
+		"menu__item",
+		"big"
+
+	).render();
+
+	new MenuCard(
+		"img/tabs/elite.jpg",
+		"elite",
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		10,
+		".menu .container",
+		"menu__item"
+
+	).render();
+
+	new MenuCard(
+		"img/tabs/post.jpg",
+		"post",
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов	животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		11,
+		".menu .container",
+		"menu__item"
+
+	).render();
+
+
+	// this в функции Конструктор
+	/* 	function User(name, id) {
+			this.name = name;
+			this.id = id;
+			this.human = true;
+		}
+
+		let ivan = new User('Ivan', 23);
+
+		console.log(ivan); */
+
 });
